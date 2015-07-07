a6a05c7a3484f6db439b73a73f0e7b5ab6e89772
giwa <ugw.gi.world@gmail.com>
2015-07-07 10:03:33 +0900

    check stdin

--- phistory.py
+++ phistory.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 import sys
 
-for line in sys.stdin:
-    splited_line = line.strip().split()[1:]
-    print("$", " ".join(splited_line))
+if sys.stdin:
+    for line in sys.stdin:
+        splited_line = line.strip().split()[1:]
+        print("$", " ".join(splited_line))
